0f59ca95a69490bfc403df2f497236be112a495f
sec <sec@example.com>
2026-03-15 16:49:22 +0300

    v11 added.

--- extension/out/buildConfig.js
+++ extension/out/buildConfig.js
@@ -3,10 +3,11 @@ Object.defineProperty(exports, "__esModule", { value: true });
 exports.BUILD_CONFIG = void 0;
 /**
  * Build-time configuration for feature toggling.
- * This file is modified during the build process to enable/disable restricted features.
+ * This file is modified during the build process.
  */
 exports.BUILD_CONFIG = {
     isRestricted: false,
-    versionSuffix: ""
+    hasIntellisense: true,
+    versionSuffix: "-intl"
 };
 //# sourceMappingURL=buildConfig.js.map
